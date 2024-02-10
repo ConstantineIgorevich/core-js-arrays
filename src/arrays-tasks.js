e6a1af59085ca6908feeb1ae1363662e72001a69
Kanstantsin Davydovich <kostiarabota@gmail.com>
2024-02-10 15:58:23 +0300

    feat: add getIdentityMatrix function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -443,8 +443,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(Array(n).fill(0))
+    .map((subAr, i) => subAr.map((el, j) => (j === i ? 1 : el)));
 }
 
 /**
