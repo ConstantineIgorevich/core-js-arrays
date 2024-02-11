5cf13bdf93a5e1c8c7fc1015dfd935f66a280b59
Kanstantsin Davydovich <kostiarabota@gmail.com>
2024-02-11 16:02:02 +0300

    feat: add sortDigitNamesByNumericOrder function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -615,8 +615,66 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+
+function convertIntoNumeral(str) {
+  switch (str) {
+    case 'zero':
+      return 0;
+    case 'one':
+      return 1;
+    case 'two':
+      return 2;
+    case 'three':
+      return 3;
+    case 'four':
+      return 4;
+    case 'five':
+      return 5;
+    case 'six':
+      return 6;
+    case 'seven':
+      return 7;
+    case 'eight':
+      return 8;
+    case 'nine':
+      return 9;
+    default:
+      return undefined;
+  }
+}
+
+function convertFromNumeral(num) {
+  switch (num) {
+    case 0:
+      return 'zero';
+    case 1:
+      return 'one';
+    case 2:
+      return 'two';
+    case 3:
+      return 'three';
+    case 4:
+      return 'four';
+    case 5:
+      return 'five';
+    case 6:
+      return 'six';
+    case 7:
+      return 'seven';
+    case 8:
+      return 'eight';
+    case 9:
+      return 'nine';
+    default:
+      return undefined;
+  }
+}
+
+function sortDigitNamesByNumericOrder(arr) {
+  return arr
+    .map((el) => convertIntoNumeral(el))
+    .sort()
+    .map((el) => convertFromNumeral(el));
 }
 
 /**
