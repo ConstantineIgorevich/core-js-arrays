95e124adf589c65df30b8abac5a89526c267546d
Kanstantsin Davydovich <kostiarabota@gmail.com>
2024-02-11 12:51:17 +0300

    feat: add getMaxItems function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -494,8 +494,19 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  let result = [];
+  arr.map((el, i) => {
+    if (i < n) {
+      result.push(el);
+    } else {
+      result = result.map((ele) => {
+        return ele < el ? el : ele;
+      });
+    }
+    return el;
+  });
+  return result.sort((a, b) => b - a);
 }
 
 /**
